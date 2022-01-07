e3a42432928a650502cee395048df3c032cfaf63
Andru Cherny <8457572+wirwolf@users.noreply.github.com>
2022-01-07 11:06:05 +0200

    Update docker-entrypoint.sh

--- src/php7.4-fpm/static/docker-entrypoint.sh
+++ src/php7.4-fpm/static/docker-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-
 set -e
 set -u
 set -o pipefail
 
+[[ ${DOCKER_DEBUG:-} ]] && set -x
 
-if [[ $1 == "bash" ]]; then
-  exec /bin/bash
+if [[ ${1:-} == "bash" ]]; then
+  exec "${@}"
   exit;
+elif [[ ${1:-} == "sh" ]]; then
+    exec "${@}"
+    exit;
 fi
 
 ###
@@ -17,20 +20,16 @@ fi
 # Path to scripts to source
 CONFIG_DIR="/docker-entrypoint.d"
 
-
-###
-### Source libs
-###
 init="$( find "${CONFIG_DIR}" -name '*.sh' -type f | sort -u )"
 for f in ${init}; do
-	# shellcheck disable=SC1090
-	echo "running $f"
-	. "${f}"
+ # shellcheck disable=SC1090
+ echo "[Entrypoint] running $f"
+ . "${f}"
 done
 
 
 echo
-echo 'Init process done. Ready for start up.'
+echo '[Entrypoint] Init process done. Ready for the start-up.'
 echo
 
 
