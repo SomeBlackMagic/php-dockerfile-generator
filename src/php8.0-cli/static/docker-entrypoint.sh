da0b6e5ed55944d7604a6d4ed31d7044ee94f0d0
Andru Cherny <wiroatom@gmail.com>
2022-02-21 16:19:01 +0200

    Refatoring docker-entrypoint.sh

--- src/php8.0-cli/static/docker-entrypoint.sh
+++ src/php8.0-cli/static/docker-entrypoint.sh
@@ -6,10 +6,10 @@ set -o pipefail
 [[ ${DOCKER_DEBUG:-} ]] && set -x
 
 if [[ ${1:-} == "bash" ]]; then
-  exec /bin/bash
+  exec "${@}"
   exit;
 elif [[ ${1:-} == "sh" ]]; then
-    exec /bin/sh
+    exec "${@}"
     exit;
 fi
 
@@ -20,20 +20,16 @@ fi
 # Path to scripts to source
 CONFIG_DIR="/docker-entrypoint.d"
 
-
-###
-### Source libs
-###
 init="$( find "${CONFIG_DIR}" -name '*.sh' -type f | sort -u )"
 for f in ${init}; do
-    # shellcheck disable=SC1090
-    echo "[Entrypoint] running $f"
-    . "${f}"
+ # shellcheck disable=SC1090
+ echo "[Entrypoint] running $f"
+ . "${f}"
 done
 
 
 echo
-echo '[Entrypoint] Init process done. Ready for start up.'
+echo '[Entrypoint] Init process done. Ready for the start-up.'
 echo
 
 
